fdda5f9575c36c089a41020e793e58a4405cbf50
James Harris <mharris9608@yahoo.com>
2021-02-16 23:11:03 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -55,4 +55,16 @@ class TicTacToe
       "O"
     end
   end
+
+  def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  if valid_move?(index)
+    move(index, current_player
+    display_board
+  else
+    turn
+  end
+end
 end
